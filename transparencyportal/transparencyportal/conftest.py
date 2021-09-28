549e5073c49897744ab86650ff674351b81a3363
Anthony BELEM <anthbel@gmail.com>
2021-09-28 14:34:33 +0000

    repo init

---
+++ transparencyportal/transparencyportal/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from transparencyportal.users.models import User
+from transparencyportal.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
